f4c295ddbed59ac99336cceebbb59d0b11cf068e
asra <sadeghiasra1378@gmail.com>
2020-12-30 09:15:24 +0330

    cssssss

--- src/App.js
+++ src/App.js
@@ -9,8 +9,8 @@ class App extends Component {
     return (
       <div className="app">
         <main>
-        
-        <currency>
+
+          <currency>
             <div>
               {
                 data.data.map((data, i) => {
@@ -18,16 +18,24 @@ class App extends Component {
                     <div key={i}>
                       <div className="currency">
                         <div >
-                          {data.title}  
-                          {data.p }    
+                          {data.title}
+                        </div>
+                        <div className="p">
+                          {data.p}
                         </div>
                         <div className="updata">
-                          
-                          اپدیت: 
+
+                          اپدیت:
                           {data.updated_at}
-                         
-                          
+
+
                         </div>
+
+                        <div class="progress">
+                          <div class="progress-bar progress-bar-striped" role="progressbar" aria-valuenow={data.dp} aria-valuemin="0" aria-valuemax="5"> {data.dp}</div>
+                        </div>
+
+
                       </div>
                     </div>
                   );
